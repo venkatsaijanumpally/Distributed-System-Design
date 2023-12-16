f76b56b24c9d74e34adb4e09e58c9478e246410f
AbhiGithub123 <abhishekkanuganti79@gmail.com>
2023-12-16 12:48:13 -0500

    Added axios

--- reactclient/src/Header/index.js
+++ reactclient/src/Header/index.js
@@ -3,61 +3,25 @@ import axios from "../helper/axios";
 
 const Header = (props) => {
   const [elasticData, setElasticData] = useState("");
+
   const getData = async () => {
-    await axios.get(`/reviews`).then(
-      (response) => {
-        setElasticData(response.data);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    await axios
+      .get(
+        `http://a1ee104f42b43436c9295211d2f68523-416343305.us-west-1.elb.amazonaws.com:8000/reviews/review/raeldor-96879/`
+      )
+      .then(
+        (response) => {
+          console.log(response.data.reviews);
+          setElasticData(response.data.reviews);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   };
   useEffect(() => {
     getData();
   }, []);
-  const reviews = [
-    {
-      _index: "reviews",
-      _type: "_doc",
-      _id: "QT7UXowB2RFwBs3zYIJ0",
-      _score: 2.5211835,
-      _source: {
-        review_detail:
-          "I enjoyed the first season, but I must say I think season 2 is even stronger. Ricky does a great job as both writer, actor and director and brings out the best in a superb supporting cast. If there was one thing I'd change, I'd like to hear him talk about himself less with other people and speak more in the third person, but other than that it's pretty hard to fault this funny yet emotional comedy.",
-        "@timestamp": "2023-12-12T16:20:25.632Z",
-        review_id: "rw5704482",
-        rating: "9",
-        spoiler_tag: 0,
-        review_summary: "Very Strong Season 2",
-        review_date: "3 May 2020",
-        reviewer: "raeldor-96879",
-        helpful: ["1", "1"],
-        "@version": "1",
-        movie: "After Life (2019– )",
-      },
-    },
-    {
-      _index: "reviews",
-      _type: "_doc",
-      _id: "SD77XowB2RFwBs3zYIJs",
-      _score: 2.5211835,
-      _source: {
-        "@timestamp": "2023-12-12T17:03:01.331Z",
-        spoiler_tag: 0,
-        movie: "Animal (2019– )",
-        review_date: "3 May 2020",
-        reviewer: "raeldor-96879",
-        review_detail:
-          "I enjoyed the first season, but I must say I think season 2 is even stronger. Ricky does a great job as both writer, actor and director and brings out the best in a superb supporting cast. If there was one thing I'd change, I'd like to hear him talk about himself less with other people and speak more in the third person, but other than that it's pretty hard to fault this funny yet emotional comedy.",
-        review_summary: "Very Strong Season 3",
-        "@version": "1",
-        rating: "9",
-        review_id: "rw5704482",
-        helpful: ["1", "1"],
-      },
-    },
-  ];
 
   return (
     <div>
@@ -68,7 +32,7 @@ const Header = (props) => {
           padding: "10px",
           textAlign: "center",
         }}>
-        <h1>My React App</h1>
+        <h1>Distributed Systems</h1>
       </header>
 
       {/* Table */}
@@ -97,28 +61,29 @@ const Header = (props) => {
             </tr>
           </thead>
           <tbody>
-            {reviews.map((row) => (
-              <tr key={row.id}>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.movie}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.reviewer}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.review_date}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.review_detail}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.review_summary}
-                </td>
-                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
-                  {row._source.rating}
-                </td>
-              </tr>
-            ))}
+            {elasticData &&
+              elasticData.map((row) => (
+                <tr key={row.id}>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.movie}
+                  </td>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.reviewer}
+                  </td>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.review_date}
+                  </td>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.review_detail}
+                  </td>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.review_summary}
+                  </td>
+                  <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                    {row._source.rating}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
